6101aece987f5f97f1c9aedd91555d5cabc00f34
maks <mamonov.maksim.20003@gmail.com>
2022-10-25 13:12:19 +0500

    задание 1.1 доработано 0.1

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,37 @@
 import java.util.HashMap;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         HashMap<String, String> guide = new HashMap<>();
-        guide.put("человек 1", "89273846182");
-        guide.put("человек 2", "88273057194");
-        guide.put("человек 3", "85195294717");
-        guide.put("человек 4", "85401759175");
-        guide.put("человек 5", "84780135893");
-        guide.put("человек 6", "89631469642");
-        guide.put("человек 7", "89273846182");
-        guide.put("человек 8", "87492418462");
-        guide.put("человек 9", "80192837461");
-        guide.put("человек 10", "81638405619");
-        guide.put("человек 11", "88267481937");
-        guide.put("человек 12", "86492746956");
-        guide.put("человек 13", "87365876984");
-        guide.put("человек 14", "82746581824");
-        guide.put("человек 15", "82654910276");
-        guide.put("человек 16", "82465982355");
-        guide.put("человек 17", "82865901550");
-        guide.put("человек 18", "83451072645");
-        guide.put("человек 19", "85991625487");
-        guide.put("человек 20", "83549917265");
-        System.out.println(guide.get("человек 1"));
-        System.out.println(guide.get("человек 2"));
-        System.out.println(guide.get("человек 3"));
-        System.out.println(guide.get("человек 4"));
-        System.out.println(guide.get("человек 5"));
-        System.out.println(guide.get("человек 6"));
-        System.out.println(guide.get("человек 7"));
-        System.out.println(guide.get("человек 8"));
-        System.out.println(guide.get("человек 9"));
-        System.out.println(guide.get("человек 10"));
-        System.out.println(guide.get("человек 11"));
-        System.out.println(guide.get("человек 12"));
-        System.out.println(guide.get("человек 13"));
-        System.out.println(guide.get("человек 14"));
-        System.out.println(guide.get("человек 15"));
-        System.out.println(guide.get("человек 16"));
-        System.out.println(guide.get("человек 17"));
-        System.out.println(guide.get("человек 18"));
-        System.out.println(guide.get("человек 19"));
-        System.out.println(guide.get("человек 20"));
+        Random random = new Random();
+        int variable = 0;
+        int variableOne = 0;
+        int variableTwo = 0;
+        String variables = "";
+        String variablesNumber = "";
+        String variablesOne = "";
+        String variablesTwo = "";
+
+        for (int j = 0; j < 20; j++) {
+            int phjneNumberVeriableOne = random.nextInt(80000);
+            int phoneNumberVeriableTwo = random.nextInt(900000);
+            if (phjneNumberVeriableOne > 7000) {
+                variableOne = phjneNumberVeriableOne;
+            }
+            if (phoneNumberVeriableTwo > 100000) {
+                variableTwo = phoneNumberVeriableTwo;
+            }
+            variablesOne = String.valueOf(variableOne);
+            variablesTwo = String.valueOf(variableTwo);
+            variablesNumber = variablesOne + variablesTwo;
+            variable ++;
+            variables = String.valueOf(variable);
+            String personality = "человек " + variables;
+            guide.put(personality, variablesNumber);
+            System.out.println(guide.get(personality));
+        }
+
+        System.out.println(guide.size());
     }
 }
